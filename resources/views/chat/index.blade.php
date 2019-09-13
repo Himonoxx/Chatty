f1f0fd379b37f3e5eb0f33335c4b4fdde7c3ce38
reita.nakatani <himono55@gmail.com>
2019-09-13 17:33:11 +0000

    chat_page_create

---
+++ resources/views/chat/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center">Chat_page</h1>
+        <div class="col-sm-4 offset-sm-4 mt-5">
+            <div class="form-group">
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action text-center">Room 1  >></a>
+                    <a href="#" class="list-group-item list-group-item-action text-center">Room 2  >></a>
+                    <a href="#" class="list-group-item list-group-item-action text-center">Room 3  >></a>
+                </div>
+            </div>
+        </div>
+    
+
+@endsection
